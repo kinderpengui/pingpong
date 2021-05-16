90c1bc57bd63be0b0b15943f4500c4447ae27e8c
kinderpengui <83168085+kinderpengui@users.noreply.github.com>
2021-05-16 15:30:01 +0200

    Add files via upload
    версия 228

--- pingpong.py
+++ pingpong.py
@@ -5,6 +5,14 @@ window = display.set_mode((500, 500))
 back = (255, 219, 38)
 window.fill(back)
 
+font.init()
+font = font.SysFont("Impact", 70)
+lose = font.render("здох 1", True, (255, 0, 0))
+lose2 = font.render("здох 2", True, (255, 0, 0))
+
+speed_x = 3
+speed_y = 3
+
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, width, height):
         super().__init__()
@@ -37,6 +45,8 @@ raketka = Player("raketka.png", 10, 400, 4.5, 90, 90)
 raketka2 = Player("raketka.png", 390, 400, 4.5, 90, 90)
 ball = GameSprite("pong.png", 220, 250, 2, 30, 30)
 
+game_over = False
+
 clock = time.Clock()
 finish = False
 game = True
@@ -51,6 +61,27 @@ while game:
         raketka.update()
         raketka2.update2()
         ball.reset()
+
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if sprite.collide_rect(raketka, ball) or sprite.collide_rect(raketka2, ball):
+            speed_x *= -1
+            speed_y *= -1
+
+        if ball.rect.y > 400 or ball.rect.y < 0:
+            speed_y *= -1
+
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose, (200, 200))
+            game_over = True
+
+        if ball.rect.x > 500:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game_over = True
+
     display.update()
     clock.tick(60)
 
